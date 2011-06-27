997d681af09ecc952728f88c1ec3c6c1b68048b9
East Agile <developers@eastagile.com>
2011-06-27 14:42:39 +0700

    Model and Views P1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd04b92274712d44b05bd6ef747dbf800d374ff0ae2a5cafef2abbd6c412fe6aece722ef6fc89d5943d4930aba782d17a022b92c0e061a8368b0ff085acbd1c8b'
